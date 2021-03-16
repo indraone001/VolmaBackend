75a0d7aef5940f5fd3de226327df008d61a8b8ba
faizaaulia <faizaaulia.id@gmail.com>
2021-03-16 14:13:35 +0700

    db migrations complete

--- knex/migrations/20210316082749_admin.js
+++ knex/migrations/20210316082749_admin.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('admin', table => {
         table.increments('id_admin');
-        table.foreign('id_mhs').references('mahasiswa.id_mhs')
+        table.integer('id_mhs').unsigned().references('mahasiswa.id_mhs')
         table.timestamps();
     })
 };
