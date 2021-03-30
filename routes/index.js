ab3c757cfd87105ff6e5762a63f1b918056612f3
indraone001 <deriindrawan124@gmail.com>
2021-03-30 09:13:38 +0700

    add crud kandidat

--- routes/index.js
+++ routes/index.js
@@ -86,4 +86,90 @@ routes.delete('/book/:id', async (req, res) => {
     }
 })
 
+routes.post('/kandidat', async (req, res) => {
+    try {
+        let id_ketua = req.body.id_ketua;
+        let id_wakil = req.body.id_wakil;
+        let no_urut = req.body.no_urut;
+        let visi = req.body.visi;
+        let misi = req.body.misi;
+
+        let id = await knex('kandidat').insert({
+            "id_ketua": id_ketua,
+            "id_wakil": id_wakil,
+            "no_urut": no_urut,
+            "visi": visi,
+            "misi": misi,
+            "created_at": knex.fn.now(),
+            "updated_at": knex.fn.now(),
+        })
+        res.status(201).send({
+            success: true,
+            data : {
+                id: id[0],
+                id_ketua,
+                id_wakil,
+                no_urut,
+                visi,
+                misi,
+            }
+        });
+    } catch (e) {
+        console.log(e);
+        next(e)
+    }
+})
+
+routes.get('/kandidat', async (req, res) => {
+    try {
+        let kandidat = await knex('kandidat');
+        res.status(200).send({
+            success: true,
+            data : kandidat,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+routes.put('/kandidat/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+        let id_ketua = req.body.id_ketua;
+        let id_wakil = req.body.id_wakil;
+        let no_urut = req.body.no_urut;
+        let visi = req.body.visi;
+        let misi = req.body.misi;
+
+        let kandidat = await knex('kandidat').where('id_kandidat', id).update({
+            "id_ketua": id_ketua,
+            "id_wakil": id_wakil,
+            "no_urut": no_urut,
+            "visi": visi,
+            "misi": misi,
+        });
+
+        res.status(201).send({
+            success: true,
+        });
+    } catch (e) {
+        console.log(e);
+        next(e)
+    }
+})
+
+routes.delete('/kandidat/:id', async (req, res) => {
+    try {
+        let id = req.params.id;
+
+        await knex('kandidat').where('id_kandidat', id).del()
+        res.status(201).send({
+            success: true,
+        });
+    } catch (e) {
+        console.log(e);
+        next(e)
+    }
+})
+
 module.exports = routes;
