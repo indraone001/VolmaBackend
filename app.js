304720eccb1463673888101804e07ba4759621a1
indraone001 <deriindrawan124@gmail.com>
2021-04-26 14:30:29 +0700

    fixed heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
-const port = 3000
+const port = 3306
 
 //body parser
 const bodyParser = require('body-parser');
