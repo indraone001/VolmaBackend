417dd063afcc3c08728f8fe30cf5895e776425d6
faizaaulia <faizaaulia.id@gmail.com>
2021-05-04 08:01:37 +0700

    migration comment

--- knex/migrations/20210316082539_kandidat.js
+++ knex/migrations/20210316082539_kandidat.js
@@ -1,5 +1,8 @@
+// table kandidat migration
 exports.up = function(knex) {
+  // create table kandidat
   return knex.schema.createTable('kandidat', table => {
+    // columns: id_kandidat, id_ketua, id_wakil, no_urut, visi, misi, time
     table.increments('id_kandidat');
     table.integer('id_ketua').unsigned().references('mahasiswa.id_mhs');
     table.integer('id_wakil').unsigned().references('mahasiswa.id_mhs');
@@ -10,6 +13,7 @@ exports.up = function(knex) {
   })  
 };
 
+// drop table if exist
 exports.down = function(knex) {
   return knex.schema.dropTable('kandidat');
 };
